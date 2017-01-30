f0b9b0d5ff467316dcd0ddaf0888ebb52e42954f
Luqman Arifin <l.arifin.siswanto@gmail.com>
2017-01-30 22:57:47 +0700

    fix bug solution

--- rank-list/sol-rank-luqman.cpp
+++ rank-list/sol-rank-luqman.cpp
@@ -17,17 +17,18 @@ int main() {
   }
   sort(p, p + n);
   reverse(p, p + n);
+  //for (int i = 0; i < n; i++) printf("%d %d\n", p[i].first, -p[i].second);
   for (int i = 0; i < n; i++) {
     int j = i;
-    while (j + 1 < n && p[i] == p[j]) j++;
+    while (j + 1 < n && p[i] == p[j + 1]) j++;
     for (int k = i; k <= j; k++) ans[k] = j - i + 1;
     i = j;
-  }  
+  }
   while (q--) {
     int k;
     scanf("%d", &k);
     k--;
-    printf("%d\n", ans[k-1]);
+    printf("%d\n", ans[k]);
   }
   return 0;
 }
